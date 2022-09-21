a4128e31b9c976fd417b9f279383e5650b22694d
Crzdd22 <trieuvy221001@gmail.com>
2022-09-21 14:38:56 +0700

    finished lab03

---
+++ app/src/main/java/com/example/intentlab/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.intentlab;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.clear:
+                AlertDialog.Builder message = new AlertDialog.Builder(this);
+                message.setTitle(R.string.message_caption);
+                message.setMessage(R.string.message_content);
+                message.setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        EditText et = (EditText) findViewById(R.id.editTextTextPersonName);
+                        et.setText("");
+
+                    }
+                }).show();
+                break;
+            case R.id.setting:
+                Intent intent = new Intent(this, optionActivity.class);
+                final int result = 1;
+                // Init activity that have return result
+                someActivityResultLauncher.launch(intent);
+                break;
+            case R.id.exit:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.exit_caption)
+                        .setMessage(R.string.exit_content)
+                        .setNegativeButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                finish();
+                            }
+                        })
+                        .setPositiveButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                return;
+                            }
+                        })
+                        .show();
+                break;
+        }
+
+        return true;
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Here, no request code
+                        Intent data = result.getData();
+// get Bundle data
+                        Bundle bundle = data.getExtras();
+                        int index1 = bundle.getInt("ForeColor");
+                        Log.i("index1", String.valueOf(index1));
+                        int index2 = bundle.getInt("BackColor");
+                        Log.i("index2", String.valueOf(index2));
+                        // get color array
+                        String colorArray[] = getResources().getStringArray(R.array.color_array);
+                        Log.i("colorArray", String.valueOf(colorArray.length));
+                        // editText mapping
+                        EditText et = (EditText) findViewById(R.id.editTextTextPersonName);
+                        // config color
+                        et.setTextColor(Color.parseColor(colorArray[index1 + 1]));
+                        et.setBackgroundColor(Color.parseColor(colorArray[index2 + 1]));
+                    }
+                }
+            });
+}
+
+
